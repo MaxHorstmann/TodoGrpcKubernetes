b30bad3174e9d16abfe73a76b6e5ec4423bf8dcd
Max Horstmann <max.horstmann@gmail.com>
2022-04-04 18:46:54 -0400

    add reflection

--- server/main.go
+++ server/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
 	pb "github.com/MaxHorstmann/TodoGrpcKubernetes/todo"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type todoserver struct {
 	pb.UnimplementedTodosServer
+}
+
+func (s *todoserver) AddTodo(ctx context.Context, todo *pb.Todo) (reply *pb.AddTodoReply, err error) {
+	fmt.Println(todo.Task)
+	fmt.Println(todo.Done)
+	return nil, nil
 }
 
 func main() {
@@ -21,6 +29,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterTodosServer(s, &todoserver{})
+	reflection.Register(s)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
